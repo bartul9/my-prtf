ededaad03cf77642d050235a587d6c5aacfe82ae
bartul_9 <bartul123@outlook.com>
2021-01-31 22:13:30 +0100

    Update script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+"use strict";
+
+const box1 = document.querySelector(".one");
+const box2 = document.querySelector(".two");
+const box3 = document.querySelector(".three");
+////
+
+const main = document.querySelector(".main");
+
+const changeImages = function (object) {
+  object.addEventListener("mouseenter", function () {
+    object.classList.add("special");
+    object.style.color = "white";
+    console.log("hover");
+  });
+
+  object.addEventListener("mouseout", function () {
+    object.classList.remove("special");
+    object.style.color = "rgba(255, 255, 255, 0)";
+  });
+};
+
+changeImages(box1);
+changeImages(box2);
+changeImages(box3);
+
+const mainfunc = function () {
+  main.addEventListener("mouseenter", function () {
+    main.style.color = "rgba(17, 0, 255, 0)";
+    main.style.backgroundImage = "url(/resize-images/head.jpg)";
+    main.style.opacity = 1;
+    setTimeout(() => {
+      main.style.backgroundImage =
+        "url(https://images.pexels.com/videos/3045163/free-video-3045163.jpg?auto=compress&cs=tinysrgb&dpr=1&w=500)";
+      main.style.color = "#03a062";
+    }, 4000);
+  });
+  //   main.addEventListener("mouseout", function () {
+  //     main.style.opacity = 0;
+  //     main.style.color = "#03a062";
+  //   });
+};
+
+mainfunc();
